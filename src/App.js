414bfa802f89eb06a22562578ccb4010fb882926
Rohul Amin <rohulamin83716@gmail.com>
2022-02-19 23:54:45 +0600

    react dark mode intregation witrh css and MUI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Switch from "@mui/material/Switch";
 
 function App() {
+  const [buttontoggle, setButtontoggle] = useState(false);
+
+  const toggle = () => {
+    if (buttontoggle == false) {
+      setButtontoggle(true);
+    } else {
+      setButtontoggle(false);
+    }
+  };
+  console.log(buttontoggle);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className={buttontoggle ? "darkMode" : "divStle"}>hello</div>
+      <button onClick={() => toggle(true)}>Dark</button>
+      <Switch onClick={() => toggle(true)} defaultChecked />
     </div>
   );
 }
